2656c28f33cbbaa09fe646a876c19130f562c4e1
Githmi Hashara <pdghashara@std.appsc.sab.ac.lk>
2024-09-04 11:22:36 +0530

    Add Hero component and update App layout

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,20 +1,21 @@
 import styles from "./App.module.css";
-// import { About } from "./components/About/About";
-// import { Contact } from "./components/Contact/Contact";
-// import { Experience } from "./components/Experience/Experience";
-// import { Hero } from "./components/Hero/Hero";
+import { About } from "./components/About/About";
+import { Contact } from "./components/Contact/Contact";
+import { Experience } from "./components/Experience/Experience";
+import { Hero } from "./components/Hero/Hero";
 import { Navbar } from "./components/Navbar/Navbar";
-// import { Projects } from "./components/Projects/Projects";
+import { Projects } from "./components/Projects/Projects";
+
 
 function App() {
   return (
     <div className={styles.App}>
       <Navbar />
-      {/* <Hero />
+      <Hero />
       <About />
       <Experience />
       <Projects />
-      <Contact /> */}
+      <Contact />
     </div>
   );
 }
--- react-portfolio/src/main.jsx
+++ react-portfolio/src/main.jsx
@@ -1,12 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+
 import "@fontsource/outfit";
 import "@fontsource/roboto";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
