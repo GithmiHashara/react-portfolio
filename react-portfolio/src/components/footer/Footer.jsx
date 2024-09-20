8860567036892fd3800b418aa3bcfbb6f76fa41b
Githmi Hashara <pdghashara@std.appsc.sab.ac.lk>
2024-09-20 19:25:42 +0530

    footer update

--- react-portfolio/src/components/footer/Footer.jsx
+++ react-portfolio/src/components/footer/Footer.jsx
@@ -1,13 +1,12 @@
+// Footer.jsx
 import styles from './FooterStyles.module.css';
 
-function Footer() {
+export const Footer = () => {
   return (
     <section id="footer" className={styles.container}>
       <p>
         &copy; 2024 Githmi Hashara <br />
-              </p>
+      </p>
     </section>
   );
-}
-
-export default Footer;
+};
