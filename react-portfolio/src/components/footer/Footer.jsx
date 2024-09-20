6cc278a143df45598c308bbf27b27326deb91d2c
Githmi Hashara <pdghashara@std.appsc.sab.ac.lk>
2024-09-20 18:12:10 +0530

    Refactor Footer component to add container class for center alignment and footer text styling

---
+++ react-portfolio/src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import styles from './FooterStyles.module.css';
+
+function Footer() {
+  return (
+    <section id="footer" className={styles.container}>
+      <p>
+        &copy; 2024 Githmi Hashara <br />
+              </p>
+    </section>
+  );
+}
+
+export default Footer;
