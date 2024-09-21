66fce4443dfb2a74c0e1ff429f26a3549597d52d
Githmi Hashara <pdghashara@std.appsc.sab.ac.lk>
2024-09-21 14:44:03 +0530

    Refactor About component to update introduction text and improve developer description

--- react-portfolio/src/components/About/About.jsx
+++ react-portfolio/src/components/About/About.jsx
@@ -3,10 +3,10 @@ import React from "react";
 import styles from "./About.module.css";
 import { getImageUrl } from "../../utils";
 
-export const About = () => {
+export const About  = () => {
   return (
     <section className={styles.container} id="about">
-      <h2 className={styles.title}>About</h2>
+      <h2 className={styles.title}>About Me</h2>
       <div className={styles.content}>
         <img
           src='assects\about\aboutImage.png'
@@ -17,10 +17,14 @@ export const About = () => {
           <li className={styles.aboutItem}>
             <img src='assects\about\cursorIcon.png' alt="Cursor icon" />
             <div className={styles.aboutItemText}>
-              <h3>Frontend Developer</h3>
+              <h3>Full Stack Developer</h3>
               <p>
-              I am a passionate Fullstack Developer and undergraduate student with a strong interest in web and game development. With experience in JavaScript, React, and Unity, I've built various projects, including an Instagram clone and a WhatsApp clone. My skills span modern web technologies like Next.js and Firebase, and I’m always eager to explore new frameworks and tools. My journey in technology is driven by a commitment to creating innovative solutions and a curiosity for the latest advancements in the field.
-              </p>
+              I’m a passionate Fullstack Developer and undergraduate student with a 
+              strong focus on web and game development. Skilled in NextJs, React,
+               and Unity, I love exploring new technologies and building creative, impactful solutions.
+                Always curious and driven to stay on top of the latest trends in tech, I'm committed
+                 to growing as a developer and delivering innovative experiences.
+             </p>
             </div>
           </li>
           {/* <li className={styles.aboutItem}>
